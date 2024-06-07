2f19a72e7b64d1ae1428c0abc7a3c71caca94c8b
vanparfait <nguemechiaparfait@gmail.com>
2024-06-07 14:33:09 +0100

    explication des composants

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import  { FunctionComponent, useEffect, useState } from 'react';
+import POKEMONS from './models/mock-pokemon';
+import  Pokemon  from './models/pokemon';
 
+
+const App: FunctionComponent = () => {
+
+  const [pokemons, setPokemon] = useState<Pokemon[]>([])
+
+  useEffect(() => {
+    setPokemon(POKEMONS)
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>il y'a {pokemons.length} pokemons!! </h1>
+      <ul>
+      {
+        pokemons.map((pok) => <li key={pok.id}>{pok.name}</li>)
+      }
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
